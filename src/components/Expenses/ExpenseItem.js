1a268aa4a67b55195a941882bc5c7af1eabb166b
Abdullah Ban <xbdullahx@gmail.com>
2023-04-11 04:01:41 +0300

    the data is dynamically displayed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,21 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle("-");
-    console.log("Clicked!!!");
-  };
+  // const clickHandler = () => {
+  //   setTitle("-");
+  //   console.log("Clicked!!!");
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{"$" + props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
