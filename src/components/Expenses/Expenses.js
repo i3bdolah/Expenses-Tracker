40f83e2aa87ac0b1580f152982aa6d1078d570e0
Abdullah Ban <xbdullahx@gmail.com>
2023-04-15 22:27:52 +0300

    Section 6 is Finished!

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -13,9 +14,15 @@ function Expenses(props) {
     setYear(comingYear);
   };
 
+  const filterdExpenses = props.data.filter(
+    (el) => el.date.getFullYear().toString() === year
+  );
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={year} onYearChange={yearChangeHandler} />
+
+      <ExpensesChart expenses={filterdExpenses} />
 
       <ExpensesList data={props.data} year={year} />
     </Card>
